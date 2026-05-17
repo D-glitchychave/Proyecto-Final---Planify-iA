15c320983fc8b4f13580bbf2fd56e644fdbf66c8
Damian Chavez <83085600+D-glitchychave@users.noreply.github.com>
2026-05-17 17:13:30 -0600

    Update header.php

--- Sitio Web - Aplicacion/includes/header.php
+++ Sitio Web - Aplicacion/includes/header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -6,21 +10,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Planify iA</title>
 
+    <!-- GOOGLE ANALYTICS -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XVKKMZNYFJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-XVKKMZNYFJ');
+    </script>
+
     <!-- ICONOS -->
     <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/4712/4712109.png">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
 
     <!-- FONTS -->
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@300;400&family=Playfair+Display:wght@600&display=swap"
         rel="stylesheet">
 
+    <!-- CARRUSEL -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
     <!-- CONTACTO -->
     <script src="https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js"></script>
 
     <!-- ESTILO -->
-    <link rel="stylesheet" href="assets/css/style2.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
