2c61a25618f2e0f19e7ec8d0f3c203fcd0141c6c
Damian Chavez <83085600+D-glitchychave@users.noreply.github.com>
2026-05-17 17:16:37 -0600

    Update registro.php

--- Sitio Web - Aplicacion/registro.php
+++ Sitio Web - Aplicacion/registro.php
@@ -5,15 +5,19 @@
 <!-- FORM -->
 <section class="register-section">
     <div class="register-box text-center">
-        <h2>Crear cuenta</h2>
-        <form action="registro.php" method="POST">
-            <input type="text" name="usuario" placeholder="Usuario..." required>
-            <input type="text" name="correo" placeholder="Correo..." required>
-            <input type="password" name="password" placeholder="Contraseña..." required>
-            <button type="submit">Registrarse</button>
+        <h1>- Planify iA -</h1>
+        <h2><u>Crear Cuenta</u></h2>
+        <form action="auth/registro_process.php" method="POST">
+            <input class="register-input" type="text" name="nombre" placeholder="Nombre..." required>
+            <input class="register-input" type="text" name="usuario" placeholder="Usuario..." required>
+            <input class="register-input" type="text" name="correo" placeholder="Correo..." required>
+            <input class="register-input" type="password" name="password" placeholder="Contraseña..." required>
+            <button class="register-button" type="submit">Registrarse</button>
         </form>
     </div>
 </section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 <?php include 'includes/footer.php'; ?>
