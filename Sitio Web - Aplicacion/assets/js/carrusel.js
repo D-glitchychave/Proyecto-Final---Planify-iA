73fdc7b6617df56d196985f752a4244fa86a6837
Damian Chavez <83085600+D-glitchychave@users.noreply.github.com>
2026-05-17 17:09:17 -0600

    Create carrusel.js

---
+++ Sitio Web - Aplicacion/assets/js/carrusel.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const swiper = new Swiper(".mySwiper", {
+
+        loop: true,
+
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+
+    });
+
+});
