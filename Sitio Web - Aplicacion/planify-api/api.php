fd0343a823f01282ace8ac82e6cc47ca40e1bb4e
Damian Chavez <83085600+D-glitchychave@users.noreply.github.com>
2026-05-18 18:18:08 -0600

    Update api.php

--- Sitio Web - Aplicacion/planify-api/api.php
+++ Sitio Web - Aplicacion/planify-api/api.php
@@ -1,27 +1,35 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 header("Content-Type: application/json");
 
 $data = json_decode(file_get_contents("php://input"), true);
 $mensaje = $data["mensaje"];
 
-$apiKey = "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";
+$apiKey = "sk-proj-...3UA";
 
-$url = "https://openrouter.ai/api/v1/chat/completions";
+$url = "https://api.openai.com/v1/chat/completions";
 
 $datos = [
-    "model" => "meta-llama/llama-3.3-70b-instruct:free",
+    "model" => "gpt-4o-mini",
+    "max_tokens" => 250,
     "messages" => [
-        ["role" => "system", "content" => "Eres una IA que ayuda a estudiantes a crear rutinas de estudio, ejercicio y productividad."],
+        ["role" => "system", "content" => "Eres una IA que ayuda a estudiantes a crear rutinas de estudio, ejercicio y productividad. Responde de forma clara, organizada y breve. Genera rutinas realistas para estudiantes universitarios. Usa listas y horarios cuando sea necesario."],
         ["role" => "user", "content" => $mensaje]
     ]
 ];
 
 $headers = [
     "Content-Type: application/json",
-    "Authorization: Bearer " . $apiKey,
-    "HTTP-Referer: https://damianchavezproyectointegral.atwebpages.com",
-    "X-Title: Planify IA"
+    "Authorization: Bearer " . $apiKey
 ];
 
 $ch = curl_init($url);
@@ -48,6 +56,15 @@ if(curl_errno($ch)){
 curl_close($ch);
 
 $resultado = json_decode($response, true);
+
+if(isset($resultado['error'])){
+
+    echo json_encode([
+        "respuesta" => "Error OpenRouter: " . $resultado['error']['message']
+    ]);
+
+    exit;
+}
 
 $respuestaIA =
 $resultado['choices'][0]['message']['content']
